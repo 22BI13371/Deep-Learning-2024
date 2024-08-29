b5042b2748648cd25a5d0b4f41d62129057eb070
Quân Nguyễn <155754824+22BI13371@users.noreply.github.com>
2024-08-29 15:45:11 +0700

    completed practical2.py

---
+++ scripts/practical2.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import math
+
+x = [i for i in range(-100, 101)]
+y = [math.pow(i, 2) for i in x]
+
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.plot(x, y)
+# plt.show()
+
+def gradientdDescent(x, error = 0.01, learningRate = 0.01, iterations = 100):
+  xArray = []
+  
+  for i in range(0, iterations):
+    x = x - learningRate * (2*x)
+    xArray.append(x)
+    if math.pow(x - i, 2) < error:
+      plt.plot(xArray)
+      plt.show()  
+      return x
+    
+  
+  
+  return x
+
+print(gradientdDescent(100, error=0.01,  iterations=5000 ) )
+  
