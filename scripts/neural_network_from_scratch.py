69805b9ea36e91fbf509d5f2c451cc6db37c74a3
Quân Nguyễn <155754824+22BI13371@users.noreply.github.com>
2024-09-17 15:42:23 +0700

    fixed neural network from scratch

--- scripts/neural_network_from_scratch.py
+++ scripts/neural_network_from_scratch.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 data_file = "data/xorData.csv"
 
-w1 = np.array([[1.5, -0.5], 
-              [-1, 1],
+w1 = np.array([[-1, 1],
               [-1, 1]])
 
-w2 = np.array([1.5, 1, 1])
+w2 = np.array([1, 1])
+
+w1_bias = np.array([1.5, -0.5])
+w2_bias = -1.5
 
 data = pd.read_csv(data_file)
 
@@ -17,21 +19,19 @@ y_train = np.asarray(data["Result"])
 # print(x_train.shape)
 # print(y_train.shape)
 
-def signmoid(x):
+def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
-def feed_forward(x, w1, w2):
-  z1 = x.dot(w1)
-  a1 = signmoid(z1)
+def feed_forward(x, w1, w2, w1_bias, w2_bias):
+  z1 = x.dot(w1) + w1_bias
+  a1 = np.round(sigmoid(z1))
   
-  a1 = np.insert(a1, 0, [1])
-  z2 = a1.dot(w2)
-  a2 = signmoid(z2)
-  return float(a2)
+  z2 = a1.dot(w2) + w2_bias
+  a2 = np.round(sigmoid(z2))
+  return round(a2)
 
 output = []
 
 for i in range(len(x_train)):
-  output.append(round(feed_forward(x_train[i], w1, w2)))
-  
+  output.append(feed_forward(x_train[i], w1, w2, w1_bias, w2_bias))
 print(output)
